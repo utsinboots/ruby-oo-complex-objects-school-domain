4cf3317ce1f93a12fd1e21c0425e6b3d32b9614a
github username <github email address>
2021-08-15 11:23:16 +0545

    commit tree 1ec956e31d9ad92ce15546d2de07019e6054e2b4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :name
+    #attr_reader :roster, :length
+    def initialize(name)
+        @name = name
+    end
+    def roster
+        @roster = {}   //empty hash
+    end    
+    def add_student(name, grade)
+        
+        @roster[grade] = name
+        binding.pry
+    end
+end
